c25f479a7c2d56cded1a3bffc8fc9e2657b4c073
zandreibonales <z.abonales.568246@umindanao.edu.ph>
2026-02-27 09:11:10 +0800

    added db context

---
+++ lab5.Infrastructure/BookstoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab5.Infrastructure
+{
+    public class BookstoreDbContext : DbContext {
+        public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options)
+           : base(options) { }
+    }
+}
---
+++ lab5.Infrastructure/BookstoreDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab5.Infrastructure
+{
+    public class BookstoreDbContextFactory : IDesignTimeDbContextFactory<BookstoreDbContext>
+    {
+        public BookstoreDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BookstoreDbContext>();
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;
+                                          Database=BookstoreDB;
+                                          Trusted_Connection=True;");
+
+            return new BookstoreDbContext(optionsBuilder.Options);
+        }
+    }
+}
